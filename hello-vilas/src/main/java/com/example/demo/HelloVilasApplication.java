44617118b1706bb25a3ed24e6fa94f50d2d3d27d
Vilas Tarte <79260184+Vilas7813@users.noreply.github.com>
2022-04-28 01:18:27 +0530

    initial hello project
    Initial Commit

---
+++ hello-vilas/src/main/java/com/example/demo/HelloVilasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloVilasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloVilasApplication.class, args);
+	}
+
+}
---
+++ hello-vilas/src/main/java/com/example/demo/RestController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController 
+{
+
+	@GetMapping("/hello")
+	String m1()
+	{
+		
+		return "Hello vilas";
+	}
+	
+	
+}
